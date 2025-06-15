a97c3ee1c7982ee62c2a84cfbf97069a5d8bd396
tushar_singh <tscorpo2@gmail.com>
2025-06-15 13:23:29 +0530

    auto_redirect_added

--- nextjsApp/src/app/enter/page.tsx
+++ nextjsApp/src/app/enter/page.tsx
@@ -6,11 +6,15 @@ import JoinQuizPopup from '@/components/joinQuiz';
 import VantaBackground from '@/components/birdsBack';
 import {v4 as uuidv4} from 'uuid'
 import LoadingUI from '@/components/loading';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
 
 export default function Enter() {
     const [showCreate , setCreate] = useState<boolean>(false);
     const [showJoin , setJoin] = useState<boolean>(false);
     const [loading,setLoading] = useState<boolean>(false);
+    const [checking , setChecking] = useState<boolean>(true);
+    const router = useRouter();
     useEffect(()=>{
         try {
             if(!localStorage.getItem("intelli-quiz-userId")) {
@@ -21,9 +25,29 @@ export default function Enter() {
             const id = uuidv4();
             localStorage.setItem("intelli-quiz-userId" , id);
         }
+
+        if(localStorage.getItem("intelli-quiz-userId") && localStorage.getItem('username')) {
+
+            const id = localStorage.getItem("intelli-quiz-userId") + '_' + localStorage.getItem('username')
+            const response = axios.get(`/api/v2/?userId=${id}`);
+            response.then((res)=>{
+
+                const data = res.data;
+                if(data.code == 1) {
+
+                    router.push(`/enter/${data.roomId}`)
+
+                }
+                else setChecking(false);
+            });
+        
+        }
+
     },[]);
 
     if(loading) return <LoadingCompo/>;
+
+    if(checking) return <EnterLoading/>
 
     return <>
     <VantaBackground value='GLOBE'>
@@ -47,7 +71,6 @@ export default function Enter() {
     </>
 }
 
-
 function LoadingCompo() {
     return <>
         <div role="status" className=" animate-pulse flex flex-col">
@@ -61,5 +84,22 @@ function LoadingCompo() {
         </div>
         <LoadingUI/>
         </div>
+    </>
+}
+
+function EnterLoading() {
+    return <>
+    <div role="status" className=" animate-pulse">
+        <div className="grid grid-cols-2 h-screen text-white bg-red-100">
+            <div className="flex flex-col gap-4 min-h-screen justify-center">
+                    <button className="border w-56 mx-auto p-4 font-bold border bg-gray-400">AI Generated Quiz</button>
+                    <button className="border w-56 mx-auto p-4 font-bold border bg-gray-400">Create AI Generated Multiplayer Quiz</button>
+                    <button className="border w-56 mx-auto p-4 font-bold border bg-gray-400">Join Multiplayer Quiz</button>
+            </div>
+            <div className="relative flex flex-col gap-4 min-h-full justify-center items-center">
+                
+            </div>
+        </div>
+    </div>
     </>
 }
